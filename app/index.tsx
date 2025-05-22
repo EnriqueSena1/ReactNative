49e9f43b281a24b5da4d9e9df33ad7068dbd3723
Enrique Sena <enriquesenacontato07@gmail.com>
2025-05-21 21:33:03 -0400

    feature-Criando pagina

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import { Image, Button,Text, TouchableOpacity, View, TextInput, ScrollView } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 font-bold">Bem vindo</Text>
+      
+        <Image
+        // className="w-32 h-32"
+        source={{uri: "https://avatars.githubusercontent.com/u/173110821?v=4"}}
+        className="w-[300px] h-[300px] mb-4 rounded-full"
+        // source={{require("Imagem do pc seguindo rota")}}
+        />
+        <Text className="text-2xl text-dark-200 font-bold">Enrique Sena</Text>
+        <TouchableOpacity >
+          <Text>Idade: 23 anos</Text>
+          <Text>Localização: Campo grande Ms</Text>
+        </TouchableOpacity>
+        <Button title="Editar Perfil"/>
+        {/* <TextInput
+        // placeholder="Digite alguma coisa"
+
+        className="border-2 border-solid rounderd-lg border-red-800"
+        /> */}
+
+
+      
     </View>
   );
 }
