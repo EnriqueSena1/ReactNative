58438ae1e81fbd4c9e9d42301a12200108c7dd1e
Enrique Sena <enriquesenacontato07@gmail.com>
2025-05-26 21:32:24 -0400

    feature: criação de loja

--- app/index.tsx
+++ app/index.tsx
@@ -2,28 +2,62 @@ import { Image, Button,Text, TouchableOpacity, View, TextInput, ScrollView } fro
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
+    <View className="flex-1 ">
       
-        <Image
-        // className="w-32 h-32"
-        source={{uri: "https://avatars.githubusercontent.com/u/173110821?v=4"}}
-        className="w-[300px] h-[300px] mb-4 rounded-full"
-        // source={{require("Imagem do pc seguindo rota")}}
-        />
-        <Text className="text-2xl mb-5 text-dark-200 font-bold">Enrique Sena</Text>
-        <TouchableOpacity >
-          <Text>Idade: 23 anos</Text>
-          <Text className="mb-5">Localização: Campo grande Ms </Text>
-        </TouchableOpacity>
-        <Button title="Editar Perfil" />
-        {/* <TextInput
-        // placeholder="Digite alguma coisa"
+        <View className="gap-3 mx-4 flex-row">
+          <View className="bg-slate-400 w-[110px] h-[120px] ">
+          <Text className="text-3xl text-dark-200 justify-center font-bold">
+            Visitas
+          </Text>
+          <Text className="text-3xl text-dark-200 font-bold">
+            1,5000
+          </Text>
+          <Text className="text-1xl text-dark-200 font-bold">
+            Ultimos 7 dias
+          </Text>
+          </View>
 
-        className="border-2 border-solid rounderd-lg border-red-800"
-        /> */}
+          <View className="bg-slate-400 w-[110px] h-[120px] ">
+          <Text className="text-3xl text-dark-200 justify-center font-bold">
+            Visitas
+          </Text>
+          <Text className="text-3xl text-dark-200 font-bold">
+            1,5000
+          </Text>
+          <Text className="text-1xl text-dark-200 font-bold">
+            Ultimos 7 dias
+          </Text>
+          </View>
 
+          <View className="bg-slate-400 w-[110px] h-[120px] ">
+          <Text className="text-3xl text-dark-200 justify-center font-bold">
+            Visitas
+          </Text>
+          <Text className="text-3xl text-dark-200 font-bold">
+            1,5000
+          </Text>
+          <Text className="text-1xl text-dark-200 font-bold">
+            Ultimos 7 dias
+          </Text>
+          </View>
+        </View>
+        <View className=" flex-col justify-center itens-center w-auto ">
+          <View className="w-[200]">
+          <Image source={{ uri: "https://a-static.mlcdn.com.br/1500x1500/jaqueta-couro-masculina-moderna-couro-resistente-slim-motoqueiro-vmong/prosperdigital/5553a4c4c72811ed87434201ac18502f/a5a2b8ae4fe16d71fb1bd28cf2a7383a.jpeg" }} className="w-[200] h-[250] "/>
+            
+          
+          <Text className="text-1xl justify-center  text-dark-200 font-bold"> Item 1
+          </Text>
+          <Text className="text-1xl justify-center  text-dark-200 font-light"> Descrição item 1, informações adicionais podem ser inseridas aqui
+          </Text>
+          
+          </View>
+        
+        </View>
 
+     
       
     </View>
+  
   );
 }
